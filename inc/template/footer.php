353fcc70c51d37967d6e812261633deeab0648af
Moysinho Sykov <moysinho.sykov@gmail.com>
2022-07-31 11:28:53 -0400

    estructura basica de php, colocando funciones basicas como el detectar en el body que pagina es para colocar la clase correspondiente a la pagina, el templates como el header y el footer

---
+++ inc/template/footer.php
@@ -0,0 +1,4 @@
+<script src="js/sweetalert2.all.min.js"></script>
+<script src="./js/app.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,23 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>UpTask</title>
-    <link rel="stylesheet" href="css/fontawesome-all.min.css">
-    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.0/normalize.css">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900" rel="stylesheet">
-    <link rel="stylesheet" href="css/sweetalert2.min.css">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
+<?php
+    include './inc/functions/functions.php';
+    include './inc/template/header.php'; 
+?>
 
 <div class="barra">
     <h1>UpTask - Administración de Proyectos</h1>
     <a href="#">Cerrar Sesión</a>
 </div>
-
 <div class="contenedor">
     <aside class="contenedor-proyectos">
         <div class="panel crear-proyecto">
@@ -57,10 +46,7 @@
             </div>
         </form>
         
- 
-
         <h2>Listado de tareas:</h2>
-
         <div class="listado-pendientes">
             <ul>
 
@@ -76,9 +62,3 @@
     </main>
 </div><!--.contenedor-->
 
-
-<script src="js/sweetalert2.all.min.js"></script>
-
-
-</body>
-</html>
