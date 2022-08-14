6ae45734ef57bc425b33935d38b68c8e8e094fb4
Moysinho Sykov <moysinho.sykov@gmail.com>
2022-08-14 09:57:49 -0400

    primer commit

--- public/js/app.js
+++
@@ -1,7 +0,0 @@
-import proyectos from './modulos/proyectos';
-import tareas from './modulos/tareas';
-import {actualizarAvance} from './funciones/avance';
-
-document.addEventListener('DOMContentLoaded', () => {
-    actualizarAvance();
-})
