dc91039f0a99ec972280a651282a81e572b2a474
Moysinho Sykov <moysinho.sykov@gmail.com>
2022-08-14 10:31:54 -0400

    estructura basica de MVC, controller

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,15 @@ const { Router } = require("express");
 const express = require("express");
 const router = express.Router();
 
+// importar el controllador
+const proyectosControllers = require('../controllers/proyectosControllers')
+
 module.exports = function () {
   // ruta para el home
-  router.get("/", (req, res) => {
-    res.send("Index");
-  });
+  router.get("/", proyectosControllers.proyectosHome);
 
   // ruta para nosotros
-  router.get("/nosotros", (req, res) => {
-    res.send("Nosotros");
-  });
+  router.get("/nosotros", proyectosControllers.nosotros);
 
   return router;
 };
